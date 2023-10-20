18b13ebee710f7b408615b1a3ed04d32e55e2e19
NTP <114249304+BIOTOC@users.noreply.github.com>
2023-10-20 15:40:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mẹ';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Mẹ';
+const contentCard = 'Chúc Mẹ ngày 20/10 tràn ngập niềm vui và hạnh phúc.';
+
+
+const giftImage = 'anh1.jpg';
+const giftImageBase64 = 'and2.jpg';
